189288b4f4d0b92dd6ca0d0fe492dc24782f2d53
Paolo Gervasoni <pgvila@hotmail.com>
2017-01-11 12:22:13 +0100

    basic working version but with wrong dynamics and wrong populating method

--- simp_agent.py
+++ simp_agent.py
@@ -1,10 +1,153 @@
 # IMPORT LIBS
+import random
+import numpy as np
 import networkx as nx
 
-class Agent:
+class Simple_Language_Agent:
+
     def __init__(self, unique_id, language, S):
         self.unique_id = unique_id
-        self.language = language
+        self.language = language # 0, 1, 2 => spa, bil, cat
         self.S = S
-    def move_random(self):
-        pass
+
+        self.lang_freq = dict()
+        self.lang_freq['spoken'] = [0, 0] # 0, 2 => spa, cat
+        self.lang_freq['heard'] = [0, 0]
+        self.lang_freq['cat_pct_s'] = 0
+        self.lang_freq['cat_pct_h'] = 0
+
+
+    def move_random(self, model):
+        """ Take a random step into any surrounding cell
+            All eight surrounding cells are available as choices
+            Current cell is not an output choice
+
+            Arguments:
+                * model : ABM model
+
+            Returns:
+                * modifies self.pos attribute
+        """
+        x, y = self.pos  # attr pos is defined when adding agent to schedule
+        possible_steps = model.grid.get_neighborhood(
+            (x, y),
+            moore=True,
+            include_center=False
+        )
+        chosen_cell = random.choice(possible_steps)
+        model.grid.move_agent(self, chosen_cell)
+
+    def speak(self, model, with_agent=None):
+        """ Pick random lang_agent from current cell and start a conversation
+            with it. It updates heard words in order to shape future vocab.
+            Language of the conversation is determined by given laws,
+            including probabilistic ones based on parameter self.S
+            This method can also simulate distance contact e.g.
+            phone, messaging, etc ... by specifying an agent through 'with_agent'
+
+            Arguments:
+                * model : ABM model
+                * with_agent : specify a specific agent with which conversation will take place
+                  By default the agent will be picked randomly from all lang agents in current cell
+
+            Returns:
+                * Defines conversation and language(s) in which it takes place.
+                  Updates heard/used stats
+        """
+        if with_agent is None:
+            pos = [self.pos]
+            # get all agents currently placed on chosen cell
+            others = model.grid.get_cell_list_contents(pos)
+            ## linguistic model of encounter with another random agent
+            if len(others) >= 1:
+                other = random.choice(others)
+                self.get_conversation_lang(other)
+        else:
+            self.get_conversation_lang(with_agent)
+            other = with_agent
+        # update lang experience
+        self.update_lang_pcts(self)
+        self.update_lang_pcts(other)
+        # check lang switch
+        self.check_lang_switch(self, model)
+        self.check_lang_switch(other, model)
+
+    def get_conversation_lang(self, other):
+        # spa-bilingual
+        if (self.language, other.language) in [(0,0),(0,1),(1,0)]:
+            for key in ['heard','spoken']:
+                self.lang_freq[key][0] += 1
+                other.lang_freq[key][0] += 1
+        # bilingual-cat
+        elif (self.language, other.language) in [(2,1),(1,2),(2,2)]:
+            for key in ['heard', 'spoken']:
+                self.lang_freq[key][1] += 1
+                other.lang_freq[key][1] += 1
+        # bilingual-bilingual
+        elif (self.language, other.language) == (1, 1):
+            # find out lang spoken by self
+            if sum(self.lang_freq['spoken']) != 0:
+                p10 = self.lang_freq['spoken'][0]/sum(self.lang_freq['spoken'])
+                p11 = 1 - p10
+                l1 = np.random.choice([0,1], p=[p10,p11])
+            else:
+                l1 = random.choice([0,1])
+            self.lang_freq['spoken'][l1] += 1
+            other.lang_freq['heard'][l1] += 1
+            # find out language spoken by other
+            self.lang_freq['heard'][l1] += 1
+            other.lang_freq['spoken'][l1] += 1
+        # spa-cat
+        else:
+            if sum(self.lang_freq['spoken']) != 0:
+                p10 = self.lang_freq['spoken'][0]/sum(self.lang_freq['spoken'])
+                p11 = 1 - p10
+                l1 = np.random.choice([0,1], p=[p10,p11])
+            else:
+                l1 = random.choice([0, 1])
+            self.lang_freq['spoken'][l1] += 1
+            other.lang_freq['heard'][l1] += 1
+            # find out language spoken by other
+            if sum(other.lang_freq['spoken']) != 0:
+                p20 = other.lang_freq['heard'][0]/sum(other.lang_freq['heard'])
+                p21 = 1 - p20
+                l2 = np.random.choice([0,1], p=[p20,p21])
+            else:
+                l2 = random.choice([0, 1])
+            self.lang_freq['heard'][l2] += 1
+            other.lang_freq['spoken'][l2] += 1
+
+    def update_lang_pcts(self, agent):
+        if sum(agent.lang_freq['spoken']) != 0:
+            agent.lang_freq['cat_pct_s'] = round(agent.lang_freq['spoken'][1] / sum(agent.lang_freq['spoken']), 2)
+        else:
+            agent.lang_freq['cat_pct_s'] = 0
+        if sum(agent.lang_freq['heard']) != 0:
+            agent.lang_freq['cat_pct_h'] = round(agent.lang_freq['heard'][1] / sum(agent.lang_freq['heard']), 2)
+        else:
+            agent.lang_freq['cat_pct_h'] = 0
+
+    def check_lang_switch(self, agent, model):
+        if model.schedule.steps > 100:
+            if agent.language == 0:
+                if agent.lang_freq['cat_pct_h'] >= 0.25:
+                    agent.language = 1
+            elif agent.language == 2:
+                if agent.lang_freq['cat_pct_h'] <= 0.75:
+                    agent.language = 1
+            else:
+                if agent.lang_freq['cat_pct_h'] >= 0.9:
+                    agent.language = 2
+                elif agent.lang_freq['cat_pct_h'] <= 0.1:
+                    agent.language = 0
+
+
+
+
+    def step(self, model):
+        self.move_random(model)
+        self.speak(model)
+
+
+    def __repr__(self):
+        return 'Lang_Agent_{0.unique_id!r}'.format(self)
--- simp_model.py
+++ simp_model.py
@@ -11,21 +11,107 @@ import matplotlib.pylab as plt
 import matplotlib.animation as animation
 from scipy.spatial.distance import pdist
 import networkx as nx
+
+# IMPORT FROM simp_agent.py
+from simp_agent import Simple_Language_Agent
+
 # IMPORT MESA LIBRARIES
 from mesa import Model
 from mesa.time import RandomActivation, SimultaneousActivation, StagedActivation
-from mesa.space import MultiGrid, ContinuousSpace
+from mesa.space import MultiGrid
 from mesa.datacollection import DataCollector
 
-class Language_Model(Model):
-    def __init__(self, num_people, width=100, height=100, 
+class Simple_Language_Model(Model):
+    def __init__(self, num_people, width=10, height=10, alpha=1.1,
                  max_people_factor=5):
-        # initial number of people
         self.num_people = num_people
-        self.alpha = alpha
         self.grid_width = width
         self.grid_height = height
-        
+        self.alpha = alpha
+        # define grid and schedule
         self.grid = MultiGrid(height, width, False)
         self.schedule = RandomActivation(self)
+
+        # INITIALIZE KNOWN PEOPLE NETWORK => label is lang spoken
+        self.known_people_network = nx.DiGraph()
+        #        self.known_people_network.add_edge('A','B', lang_spoken = 'cat')
+        #        self.known_people_network.add_edge('A','C', lang_spoken = 'spa')
+        #        self.known_people_network['A']['C']['lang_spoken']
+        #        self.known_people_network['B']['A'] = 'cat'
+
+        # INITIALIZE FRIENDSHIP NETWORK
+        self.friendship_network = nx.Graph()  # sort by friendship intensity
+        #       sorted(self.friendship_network[n_1].items(),
+        #                    key=lambda edge: edge[1]['link_strength'],
+        #                    reverse = True)
+
+        # INITIALIZE FAMILY NETWORK
+        self.family_networks = nx.DiGraph()
+
+        S = 0.5
+        for id_, coord in zip(range(num_people), itertools.product(range(self.grid_width),
+                                                                   range(self.grid_height))
+                              ):
+            lang = np.random.choice([0,1,2], p=[0.25,0.65,0.1])
+            a = Simple_Language_Agent(id_, lang, S)
+            self.add_agent(a, coord)
+
+        # Data Collector
+        self.datacollector = DataCollector(
+            model_reporters={"count_lang": lambda m: m.get_lang_stats()}
+        )
+
+
+    def add_agent(self, a, coords):
+        # add agent to grid and schedule
+        self.schedule.add(a)
+        self.grid.place_agent(a, (coords[0], coords[1]))
+        ## add agent node to all networks
+        self.known_people_network.add_node(a)
+        self.friendship_network.add_node(a)
+        self.family_networks.add_node(a)
+
+
+    def visualize_agents_attrs(self, ag_attr):
+        """Plots linguistic agents attributes on a 2D grid
+
+        Args:
+            * ag_attr : a quantifiable attribute of the lang agent class
+        """
+
+        ag_and_coords = [(getattr(ag, ag_attr), ag.pos[0], ag.pos[1])
+                         for ag in self.schedule.agents]
+        ag_and_coords = np.array(ag_and_coords)
+        df_exper = pd.DataFrame({'x': ag_and_coords[:, 1],
+                                 'y': ag_and_coords[:, 2]})
+        df_exper['values'] = ag_and_coords[:, 0]
+        df_avg = df_exper.groupby(['x', 'y']).mean()
+        masked_array = np.ma.array(df_avg.unstack(), mask=np.isnan(df_avg.unstack()))
+        cmap = matplotlib.cm.viridis
+        cmap.set_bad('white', 1.)
+        plt.pcolor(masked_array, cmap=cmap)
+        plt.colorbar()
+        plt.title(ag_attr)
+        plt.show()
+
+    def get_lang_stats(self):
+        ag_lang_list = [ag.language for ag in self.schedule.agents]
+        lang_counts = Counter(ag_lang_list)
+        return lang_counts[1]
+
+
+    def step(self):
+        #self.get_lang_stats()
+        self.datacollector.collect(self)
+        self.schedule.step()
+
+
+    def run_model(self, steps):
+        for _ in range(steps):
+            self.step()
+
+
+
+
+
         
