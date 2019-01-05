c29f934c2d278f2f43899a91bd90c35007cbd8dd
Paolo Gervasoni <pgvila@hotmail.com>
2019-01-05 16:48:46 +0100

    Test lang choice of family members when talking to a monolingual stranger
    test_model.py: 1) Created new fixture 'family_and_stranger_ags' to select a parent,
    an adolescent child as well as a monolingual stranger 2) Created
    new 'test_get_conv_params2' to check than in neutral mode family members switch
    from their usual language in order to adapt to monolingual

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,7 +6,32 @@ from bilangsim import LanguageModel
 
 @pytest.fixture(scope="module")
 def model():
-    return LanguageModel(100, num_clusters=1, init_lang_distrib=[0.25, 0.5, 0.25])
+    return LanguageModel(300, num_clusters=1, init_lang_distrib=[0.25, 0.5, 0.25])
+
+
+@pytest.fixture(scope="module")
+def family_and_stranger_ags(model):
+    class Found(Exception):
+        pass
+    try:
+        for ag in model.schedule.agents:
+            if type(ag) == Adult and ag.info['language'] == 1:
+                pct1 = ag.lang_stats['L1']['pct'][ag.info['age']]
+                pct2 = ag.lang_stats['L2']['pct'][ag.info['age']]
+                if pct2 > pct1 > 0.5:
+                    for ch in ag.get_family_relative('child'):
+                        if type(ch) == Adolescent and ch.lang_stats['L1']['pct'][ch.info['age']] >= 0.3:
+                            if model.nws.family_network[ag][ch]['lang'] == 1:
+                                raise Found
+    except Found:
+        parent, child = ag, ch
+
+    for strg in model.schedule.agents:
+        if type(strg) == Adult and strg.info['language'] == 0:
+            if strg.lang_stats['L2']['pct'][strg.info['age']] <= 0.05:
+                stranger = strg
+
+    return parent, child, stranger
 
 
 @pytest.fixture(scope="module", params=[(457, 3), (521, 2), (897, 5), (1501, 6)])
@@ -46,11 +71,12 @@ test_data_remove_after_death = [Teacher, Adult, Adolescent]
 test_data_remove_from_locations = [False, True]
 
 
+@pytest.mark.skip(reason="too long and currently disabled")
 def test_model_consistency(model_param):
     """ Check that all agents live in the same
         cluster as that of their occupation """
     for ag in model_param.schedule.agents:
-        if ag.info['age'] > 36:
+        if ag.info['age'] > model_param.steps_per_year:
             if isinstance(ag, Young):
                 if ag.loc_info['job']:
                     if isinstance(ag, Teacher):
@@ -87,7 +113,6 @@ def test_run_conversation(model):
     assert ag_init not in kn_p_nw[others[1]]
 
 
-
 @pytest.mark.parametrize("langs, pcts_1, pcts_2, delete_edges, expected", 
                          test_data_get_conv_params)
 def test_get_conv_params(model, langs, pcts_1, pcts_2, delete_edges, expected): 
@@ -105,6 +130,12 @@ def test_get_conv_params(model, langs, pcts_1, pcts_2, delete_edges, expected):
     assert np.all(expected == (params['lang_group'], params['mute_type']))
 
 
+def test_get_conv_params2(model, family_and_stranger_ags):
+    parent, child, stranger = family_and_stranger_ags
+    params = model.get_conv_params([parent, child, stranger])
+    assert params['lang_group'] == (0, 0, 0)
+    params = model.get_conv_params([parent, child])
+    assert params['lang_group'] == (1, 1)
 
 
 @pytest.mark.parametrize("agent_type", test_data_remove_after_death)
